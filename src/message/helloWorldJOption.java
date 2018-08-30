21f0364e0bcae8be9d970fdd0348032236e29d42
Paulo Correa <paulo@localhost.localdomain>
2018-08-30 13:43:15 -0300

    enviando para o git

---
+++ src/message/helloWorldJOption.java
@@ -0,0 +1,13 @@
+package message;
+
+import javax.swing.JOptionPane;
+
+public class helloWorldJOption {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Hello World");
+		
+	}
+
+}
